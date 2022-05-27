ec06fdc51de4e751a1670de3ebf649a5460f4030
jess0204 <91644381+jess0204@users.noreply.github.com>
2022-05-27 16:44:20 +1200

    moving image plus more additives
    Add js for our moving image! YAY got it working. Added a JS  for that

---
+++ carousel.js
@@ -0,0 +1,28 @@
+
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+
+// Image List
+images[0] = "image/apple.png";
+images[1] = "image/samsung2.gif";
+images[2] = "image/logo.png";
+images[3] = "image/logo.png";
+
+// Change Image
+function changeImg(){
+document.slide.src = images[i];
+
+// Check If Index Is Under Max
+if(i < images.length - 1){
+i++; // Add 1 to Index
+} else { 
+i = 0; // Reset Back To O
+}
+
+// Run function every x seconds
+setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
